1696f88fc4f6e2e5b8b63d762fc09f03c78cc870
Erin Casseapey <friskytenno@gmail.com>
2017-07-29 11:12:45 +0800

    corrections
    Added = to the latter part of the range

--- P2/grade.cpp
+++ P2/grade.cpp
@@ -14,35 +14,35 @@ void main()
     {
         cout<<"OUT OF RANGE.";
     }
-    else if ((grade>=75) && (grade<76))
+    else if ((grade>=75) && (grade<=76))
     {
         cout<<grade<<" has a grade equivalent of 3.00.";
     }
-    else if ((grade>=77) && (grade<79))
+    else if ((grade>=77) && (grade<=79))
     {
         cout<<grade<<" has a grade equivalent of 2.75.";
     }
-    else if ((grade>=80) && (grade<81))
+    else if ((grade>=80) && (grade<=81))
     {
         cout<<grade<<" has a grade equivalent of 2.50.";
     }
-    else if ((grade>=82) && (grade<84))
+    else if ((grade>=82) && (grade<=84))
     {
         cout<<grade<<" has a grade equivalent of 2.25.";
     }
-    else if ((grade>=85) && (grade<88))
+    else if ((grade>=85) && (grade<=88))
     {
         cout<<grade<<" has a grade equivalent of 2.00.";
     }
-    else if ((grade>=89) && (grade<91))
+    else if ((grade>=89) && (grade<=91))
     {
         cout<<grade<<" has a grade equivalent of 1.75.";
     }
-    else if ((grade>=92) && (grade<94))
+    else if ((grade>=92) && (grade<=94))
     {
         cout<<grade<<" has a grade equivalent of 1.50.";
     }
-    else if ((grade>=95) && (grade<97))
+    else if ((grade>=95) && (grade<=97))
     {
         cout<<grade<<" has a grade equivalent of 1.25.";
     }
